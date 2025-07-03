314fa7702429dad3ecef972367afd8b98ced8d6f
Suraj Chaudhary <surajchaudhary6613@gmail.com>
2025-07-03 19:44:23 +0530

    Integrated frontend backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const aiRoutes = require("./routes/ai.routes");
-
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use("/ai", aiRoutes);
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import "prismjs/themes/prism-tomorrow.css";
+import prism from "prismjs";
+import { useEffect, useState } from "react";
+import Editor from "react-simple-code-editor";
+import axios from "axios";
+import Markdown from "react-markdown";
+import rehypeHighlight from 'rehype-highlight'
+import 'highlight.js/styles/github.css';
+
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const [code, setCode] = useState(`function sum() {
+   return a + b 
+}`);
+  const [review, setReview] = useState("");
+  useEffect(() => {
+    prism.highlightAll();
+  }, []);
 
-export default App
+  async function reviewCode() {
+    const response = await axios.post("http://localhost:3000/ai/get-review", {
+      code,
+    });
+    setReview(response.data);
+  }
+  return (
+    <>
+      <main className="flex h-screen p-2 bg-gray-700 gap-2 text-white">
+        <div className="left w-1/2  bg-[#000] h-full rounded-md relative">
+          <div className="code h-full">
+            <Editor
+              value={code}
+              onValueChange={(code) => setCode(code)}
+              highlight={(code) =>
+                prism.highlight(code, prism.languages.javascript, "javascript")
+              }
+              style={{
+                fontFamily: '"Fira code","Fira Mono",monospace',
+                fontSize: 16,
+                border: "1px solid #ddd",
+                borderRadius: "5px",
+                height: "100%",
+              }}
+            ></Editor>
+          </div>
+          <div
+            onClick={reviewCode}
+            className="review absolute bottom-3 right-3 p-2 border-2 bg-blue-300 rounded-md px-3 cursor-pointer"
+          >
+            Review
+          </div>
+        </div>
+        <div className="right w-1/2 bg-gray-900 rounded-md h-full overflow-auto p-4">
+          <Markdown rehypePlugins={[rehypeHighlight]}>{review}</Markdown>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default App;
