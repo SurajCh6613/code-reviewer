d2468cca3ef2f0ac7dc08d647d44a13faba1beda
Suraj Chaudhary <surajchaudhary6613@gmail.com>
2025-07-03 18:10:14 +0530

    Backend Setup

---
+++ backend/src/controllers/ai.controller.js
@@ -0,0 +1,12 @@
+const generateContent = require("../services/ai.services");
+
+module.exports.getReview = async (req, res) => {
+  const code = req.body.code;
+  if (!code) {
+    return res.status(400).send("Prompt is required");
+  }
+
+  const response = await generateContent(code);
+
+  res.send(response);
+};
